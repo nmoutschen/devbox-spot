8f9ab65e810f7dffd607029a80277bc8f0820652
Nicolas Moutschen <nicolas.moutschen@gmail.com>
2020-01-18 17:49:33 +0100

    feat: enforce idempotency on instance start

--- src/api_start/main.py
+++ src/api_start/main.py
@@ -1,6 +1,7 @@
 import json
 import os
 import boto3
+import botocore.exceptions
 
 
 ec2 = boto3.resource("ec2")
@@ -12,24 +13,37 @@ def handler(event, _):
     ip = event["requestContext"]["identity"]["sourceIp"]
 
     # Authorize the caller to send ICMP & SSH requests
-    # TODO: ignore errors when the rules already exist
     sg = ec2.SecurityGroup(os.environ["SECURITY_GROUP_ID"])
-    sg.authorize_ingress(
-        CidrIp="{}/32".format(ip),
-        IpProtocol="icmp",
-        FromPort=-1,
-        ToPort=-1
-    )
-    sg.authorize_ingress(
-        CidrIp="{}/32".format(ip),
-        IpProtocol="tcp",
-        FromPort=22,
-        ToPort=22
-    )
+    try:
+        sg.authorize_ingress(
+            CidrIp="{}/32".format(ip),
+            IpProtocol="icmp",
+            FromPort=-1,
+            ToPort=-1
+        )
+    except botocore.exceptions.ClientError as exc:
+        # Only ignore duplicate errors
+        if exc.response['Error']['Code'] != 'InvalidPermission.Duplicate':
+            raise exc
+    try:
+        sg.authorize_ingress(
+            CidrIp="{}/32".format(ip),
+            IpProtocol="tcp",
+            FromPort=22,
+            ToPort=22
+        )
+    except botocore.exceptions.ClientError as exc:
+        # Only ignore duplicate errors
+        if exc.response['Error']['Code'] != 'InvalidPermission.Duplicate':
+            raise exc
 
     # Start the instance
-    instance = ec2.Instance(os.environ["INSTANCE_ID"])
-    instance.start()
+    # Check that the instance is not already running or starting (pending)
+    if instance.state["Name"] not in ["running", "pending"]:
+        instance = ec2.Instance(os.environ["INSTANCE_ID"])
+        instance.start()
+
+    # Return the instance status and DNS
     return {
         "statusCode": 200,
         "body": json.dumps({
